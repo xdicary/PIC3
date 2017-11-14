c6308c33428348137a53d84a5292487a8080f9d9
xdicary <727119340@qq.com>
2017-11-14 14:56:34 +0800

    improved tdma

--- tdma.c
+++ tdma.c
@@ -10,55 +10,63 @@
 void tdma(void)
 {
     double a[nx];
-    double b[nx+1];
+    double b[nx];
     double c[nx];
+    double d[nx];
+
+    double q[nx];
+    double u[nx];
+    double t[nx];
+    double h[nx];
+    double g[nx];
     int i;
-    double m, test;
+    double p, test;
 
-/*    for (i=0; i<nx; i++)
+    for (i=0; i<nx; i++)
 	a[i] = -1.0;
-    a[nx] = 0.0;
 
-    for (i=0; i<=nx; i++)
+    for (i=0; i<nx; i++)
 	b[i] = 2.0;
-    b[nx+1] = 1.0;
 
-    for (i=1; i<=nx; i++)
+    for (i=0; i<nx; i++)
 	c[i] = -1.0;
-*/
-    for (i=0; i<nx-1; i++)
-	a[i] = -1.0;
-    a[nx-1] = nx;
-
-    for (i=1; i<nx; i++)
-	b[i] = 2.0;
-    b[0] = 1.0;
-    b[nx] = -nx;
-
-    for (i=1; i<nx; i++)
-	c[i] = -1.0;
-    c[0] = 0.0;
 /* 
-    for(i=0; i<nx; i++)
+    for(i=0; i<=nx; i++)
     {
 	test = 2.0*pi*i/nx;
 	rhot[i] = cos(test);
     }
 */
-    for(i=1; i<nx; i++)
-        phi[i] = rhot[i]*dx*dx;
-    phi[0] = 0.0;
-    phi[nx] = 0.0;
-    for(i=3; i<=nx; i++)
-	phi[nx] = phi[nx]+(i-2)*rhot[i-1]*dx*dx;
+    for (i=0; i<nx; i++)
+        d[i] = rhot[i+1]*dx*dx;
 
-    for (i=1; i<nx; i++)
+    p = b[0];
+    q[0] = -c[0]/p;
+    t[0] = -a[0]/p;
+    u[0] = d[0]/p;
+
+    for (i=1; i<nx-1; i++)
     {
-	m = 1.0/( b[i] - a[i-1]*c[i-1] );
-        c[i] = c[i]*m;
-	phi[i] = ( phi[i] - a[i-1]*phi[i-1] )*m;
+	p = a[i]*q[i-1]+b[i];
+	u[i] = (d[i]-a[i]*u[i-1])/p;
+	q[i] = -c[i]/p;
+	t[i] = -a[i]*t[i-1]/p;
     }
-    phi[nx] = ( phi[nx] - a[nx-1]*phi[nx-1] )/( b[nx] - a[nx-1]*c[nx-1] );
-    for (i=nx-1; i>0; i--)
-	phi[i] = phi[i] - c[i]*phi[i+1];
+
+    h[nx-1] = (d[nx-1]-a[nx-1]*u[nx-2])/(a[nx-1]*(q[nx-2]+t[nx-2])+b[nx-1]);
+    g[nx-1] = -1.0*c[nx-1]/(a[nx-1]*(q[nx-2]+t[nx-2])+b[nx-1]);
+
+    h[nx-2] = u[nx-2]+(q[nx-2]+t[nx-2])*h[nx-1];
+    g[nx-2] = (q[nx-2]+t[nx-2])*g[nx-1];
+
+    for (i=nx-3; i>0; i--)
+    {
+	h[i] = u[i]+q[i]*h[i+1]+t[i]*h[nx-1];
+	g[i] = q[i]*g[i+1]+t[i]*g[nx-1];
+    }
+
+    phi[1] = (d[0]-c[0]*h[1]-a[0]*h[nx-1])/(b[0]+c[0]*g[1]+a[0]*g[nx-1]);
+    for (i=1; i<nx; i++)
+	phi[i+1] = h[i]+g[i]*phi[1];
+    phi[0] = phi[nx];
 }
